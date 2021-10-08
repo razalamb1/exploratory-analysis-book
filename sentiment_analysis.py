c3f53cfad6a1a698bbf21d5284b1c227753cbc0d
Raza Lamb <rhlamb95@gmail.com>
2021-10-08 15:39:31 +0000

    final python file

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,25 +1,45 @@
 import boto3
-import nltk
 from pathlib import Path
 import math
 import pandas as pd
+import re
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 client = boto3.client('comprehend')
 grant_book = Path('pg4367.txt').read_text()
-grant_book = grant_book[0:200000]
+chapters = re.split(r"\n{4,5}CHAPTER [IXV]+\.", grant_book)
 
-length = len(grant_book)
-iterations = math.ceil(length/4000)
-sentiment = pd.DataFrame(columns = ['Count', 'Sentiment', 'Positive', 'Negative', 'Neutral', 'Mixed'])
 
-for i in range(iterations):
-    lower_bound = i*4000
-    upper_bound = (i+1)*4000 + 1
-    temp_text = grant_book[lower_bound:upper_bound]
-    response = client.detect_sentiment(Text=temp_text, LanguageCode = 'en')
-    score = response['SentimentScore']
-    sentiment = sentiment.append({'Count': i, 'Sentiment': response['Sentiment'], 'Positive': score['Positive'], 'Negative': score['Negative'], 'Neutral': score['Neutral'], 'Mixed':score['Mixed']}, ignore_index = True)
+
+def chapter_sentiment(chapter):
+    length = len(chapter)
+    iterations = math.ceil(length/4000)
+    sentiment = pd.DataFrame(columns =  ['Length','Positive', 'Negative', 'Neutral', 'Mixed'])
+    for i in range(iterations):
+        lower_bound = i*4000
+        upper_bound = (i+1)*4000 + 1
+        temp_text = chapter[lower_bound:upper_bound]
+        num = len(temp_text)
+        response = client.detect_sentiment(Text=temp_text, LanguageCode = 'en')
+        score = response['SentimentScore']
+        score['Length'] = num
+        sentiment = sentiment.append(score, ignore_index = True)
+        pass
+    sentiment.loc[:,'Length'] = sentiment.loc[:,'Length']/sentiment.loc[:,'Length'].sum()
+    sentiment = sentiment.iloc[:,1:].multiply(sentiment.iloc[:,0], axis="index")
+    sentiment = sentiment.sum(axis = 0)
+    return sentiment.to_dict()
+    
+
+chapters = chapters[0:4]
+final = pd.DataFrame(columns =  ['Chapter','Positive', 'Negative', 'Neutral', 'Mixed'])
+for i in range(len(chapters)):
+    sentiment = chapter_sentiment(chapters[i])
+    sentiment['Chapter'] = f'Chapter {i}'
+    final = final.append(sentiment, ignore_index=True)
     pass
-
-
-print(sentiment)
+fig, ax = plt.subplots(1, 1)
+fig= final.plot.line()
+fig.savefig('test.svg')
