13cbd65731a38c5f43032fbfdb8f9db59fffca80
Raza Lamb <rhlamb95@gmail.com>
2021-10-08 03:43:49 +0000

    working on langauge analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,9 +1,25 @@
 import boto3
 import nltk
 from pathlib import Path
+import math
+import pandas as pd
 
 client = boto3.client('comprehend')
-!wget https://gutenberg.org/cache/epub/4367/pg4367.txt
 grant_book = Path('pg4367.txt').read_text()
- 
-response = client.detect_sentiment(Text=grant_book, LanguageCode = 'en')
+grant_book = grant_book[0:200000]
+
+length = len(grant_book)
+iterations = math.ceil(length/4000)
+sentiment = pd.DataFrame(columns = ['Count', 'Sentiment', 'Positive', 'Negative', 'Neutral', 'Mixed'])
+
+for i in range(iterations):
+    lower_bound = i*4000
+    upper_bound = (i+1)*4000 + 1
+    temp_text = grant_book[lower_bound:upper_bound]
+    response = client.detect_sentiment(Text=temp_text, LanguageCode = 'en')
+    score = response['SentimentScore']
+    sentiment = sentiment.append({'Count': i, 'Sentiment': response['Sentiment'], 'Positive': score['Positive'], 'Negative': score['Negative'], 'Neutral': score['Neutral'], 'Mixed':score['Mixed']}, ignore_index = True)
+    pass
+
+
+print(sentiment)
